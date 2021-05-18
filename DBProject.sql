d4a76fa9bae70cf563c8a226656cec3ee8a757f0
Felix Prima <felix.prima@binus.ac.id>
2021-05-18 17:09:50 +0700

    Initial Commit

---
+++ DBProject.sql
@@ -0,0 +1,143 @@
+CREATE DATABASE DBProject
+GO
+
+USE DBProject
+GO
+
+CREATE TABLE Customer (
+	CustomerID		CHAR(5) NOT NULL,
+	[Name]			VARCHAR(255),
+	Email			VARCHAR(255),
+	[Address]		VARCHAR(255),
+	Date_of_Birth	VARCHAR(255),
+	Gender			VARCHAR(255),
+	PhoneNumber		VARCHAR(255),
+	[Password]		VARCHAR(255),
+	MoneyBalance	INTEGER
+	
+	CONSTRAINT PK_Customer PRIMARY KEY(CustomerID),
+	CONSTRAINT Check_CustID CHECK(CustomerID LIKE 'CU[0-9][0-9][0-9]'),
+	CONSTRAINT Check_CustName CHECK([Name] LIKE '% %'),
+	CONSTRAINT Check_CustGender CHECK(Gender LIKE 'Male' OR Gender LIKE 'Female'),
+	CONSTRAINT Check_CustEmail CHECK(Email LIKE '%@%'),
+	CONSTRAINT Check_CustNumber CHECK(LEN(PhoneNumber) = 12)
+)
+GO
+
+CREATE TABLE DBMSSoftware(
+	DBMSSoftwareID		CHAR(5) NOT NULL,
+	[Name]				VARCHAR(255),
+	[URL]				VARCHAR(255)
+	
+	CONSTRAINT PK_DBMSSoftware PRIMARY KEY(DBMSSoftwareID),
+	CONSTRAINT Check_DBMS_ID CHECK(DBMSSoftwareID LIKE 'DS[0-9][0-9][0-9]')
+)
+GO
+
+CREATE TABLE [Database](
+	DatabaseID			CHAR(5)		NOT NULL,
+	DBMSSoftwareID		CHAR(5)		NOT NULL,
+	Storage				INT,
+	Price				INT
+	
+	CONSTRAINT PK_Database PRIMARY KEY(DatabaseID),
+	CONSTRAINT FK_Database FOREIGN KEY(DBMSSoftwareID) REFERENCES DBMSSoftware,
+	CONSTRAINT Check_DB_ID CHECK(DBMSSoftwareID LIKE 'DB[0-9][0-9][0-9]'),
+	CONSTRAINT Check_DB_Storage CHECK(Storage > 0)
+)
+GO
+
+CREATE TABLE Operating_System (
+	OperatingSystemID		CHAR(5) NOT NULL,
+	[Name]					VARCHAR(255),
+	Family					VARCHAR(255),
+	Price					INTEGER
+
+	CONSTRAINT PK_OperatingSystem PRIMARY KEY(OperatingSystemID),
+	CONSTRAINT Check_OSID CHECK(OperatingSystemID LIKE 'OS[0-9][0-9][0-9]'),
+	CONSTRAINT Check_OSPrice CHECK (Price >= 500000 AND Price <= 1500000)
+)
+
+GO
+
+CREATE TABLE Processor(
+	ProcessorID		CHAR(5) NOT NULL,
+	[Name]			VARCHAR(255),
+	Cores			INTEGER,
+	BaseClockSpeed	INTEGER,
+	BoostClockSpeed	INTEGER
+
+	CONSTRAINT PK_Processor PRIMARY KEY(ProcessorID),
+	CONSTRAINT Check_ProcessorID CHECK(ProcessorID LIKE 'PR[0-9][0-9][0-9]')
+
+)
+
+GO
+
+CREATE TABLE [Server](
+	ServerID			CHAR(5) NOT NULL,
+	ProcessorID			CHAR(5) NOT NULL,
+	OperatingSystemID	CHAR(5) NOT NULL,
+	[Name]				VARCHAR(255),
+	Memory				INTEGER,
+	Price				INTEGER,
+	Storage				INTEGER
+
+	CONSTRAINT PK_Server PRIMARY KEY(ServerID),
+	CONSTRAINT FK_Server1 FOREIGN KEY(ProcessorID) REFERENCES Processor,
+	CONSTRAINT FK_Server2 FOREIGN KEY(OperatingSystemID) REFERENCES Operating_System,
+	CONSTRAINT Check_ServerID CHECK(ServerID LIKE 'SV[0-9][0-9][0-9]'),
+	CONSTRAINT Check_ServerMemory CHECK(Memory % 2 = 0),
+	CONSTRAINT Check_ServerPrice CHECK(Price >= 10000 AND Price <= 300000)
+)
+GO
+
+CREATE TABLE Cloud_Provider(
+	CloudProviderID		CHAR(5) NOT NULL,
+	[Name]				VARCHAR(255),
+	Email				VARCHAR(255),
+	Region				VARCHAR(255),
+	[URL]				VARCHAR(255)
+
+	CONSTRAINT PK_CloudProvider PRIMARY KEY(CloudProviderID),
+	CONSTRAINT Check_ProviderID	CHECK(CloudProviderID LIKE 'CP[0-9][0-9][0-9]')
+)	
+GO
+
+CREATE TABLE [Transaction](
+	TransactionID	CHAR(5) NOT NULL,
+	CustomerID		CHAR(5) NOT NULL,
+	CloudProviderID	CHAR(5) NOT NULL,
+	DatabaseID		CHAR(5) NOT NULL,
+	ServerID		CHAR(5) NOT NULL,
+	PaymentDate		DATE
+	
+	CONSTRAINT PK_Transaction PRIMARY KEY (TransactionID),
+	CONSTRAINT FK_Transaction1 FOREIGN KEY (CustomerID) REFERENCES Customer,
+	CONSTRAINT FK_Transaction2 FOREIGN KEY (CloudProviderID) REFERENCES Cloud_Provider,
+	CONSTRAINT FK_Transaction3 FOREIGN KEY (DatabaseID) REFERENCES [Database],
+	CONSTRAINT FK_Transaction4 FOREIGN KEY (ServerID) REFERENCES [Server],
+	CONSTRAINT Check_TransactionID CHECK(TransactionID LIKE 'TR[0-9][0-9][0-9]')
+)
+
+-- PROCEDURES --
+CREATE PROCEDURE Input_Customer @customer_id char(5), @name varchar(255), @email varchar(255), @address varchar(255), @date_of_birth date, @gender varchar(255), @phone_number varchar(255), @password varchar(255)
+	as INSERT INTO Customer VALUES(@customer_id, @name , @email , @address , @date_of_birth, @gender, @phone_number ,@password, 0)
+
+GO
+
+-- PROCEDURES --
+
+---- DROPS --
+--DROP TABLE Customer
+--DROP TABLE [Transaction]
+--DROP PROCEDURE Input_Customer
+--DROP TABLE DBMSSoftware
+---- DROPS --
+
+---- SELECTS --
+select * from Customer
+--SELECT LEN(PhoneNumber) FROM Customer
+---- SELECTS --
+
+--DROP DATABASE DBProject
---
+++ DML_DBProject.sql
@@ -0,0 +1,20 @@
+USE DBProject
+
+exec Input_Customer 'CU001', 'Felix Prima', 'felix.prima@binus.ac.id', 'Binus Square', '2001/07/12','Male', '081188119111', 'Udin123' 
+exec Input_Customer 'CU002', 'Budi Lopez', 'budi.lopez@binus.ac.id', 'Kemanggisan', '2004/04/28','Male', '082160143321', 'Oper928'
+exec Input_Customer 'CU003', 'Markocop Lestari', 'markocop.lestari@binus.ac.id', 'Pluit', '1999/01/01','Male', '082163307160', 'Ipoq192'
+exec Input_Customer 'CU004', 'Ricky Baswedan', 'ricky.baswedan@binus.ac.id', 'Binus Square', '2002/08/02','Male', '082272011641', 'uqiw201'
+exec Input_Customer 'CU005', 'Suryono Ivana', 'suryono.ivana@binus.ac.id', 'Kelapa Gading', '2012/02/03','Male', '082183019736', 'Udne022'
+exec Input_Customer 'CU006', 'Gatot Kamal', 'gatot.kamal@binus.ac.id', 'Bandengan', '2010/10/10','Male', '082252303112', 'Pass122'
+exec Input_Customer 'CU007', 'Iskan Noviandri', 'iskan.noviandri@binus.ac.id', 'Lestari', '2009/09/09','Female', '082216036969', 'Ciee102'
+exec Input_Customer 'CU008', 'Okta Firma', 'okta.firma@binus.ac.id', 'Cawang Barat', '2004/07/04','Female', '082173891311', 'aduh192'
+exec Input_Customer 'CU009', 'Alkane Ikrum', 'alkane.ikrum@binus.ac.id', 'Cawang Pusat', '1998/12/12','Male', '082161304108', 'sars029'
+exec Input_Customer 'CU010', 'Petrus Imba', 'petrus.imba@binus.ac.id', 'Cawang Timur', '2006/06/12','Male', '083184115883', 'ampe862'
+exec Input_Customer 'CU011', 'Metal Alingga', 'metal.alingga@binus.ac.id', 'Bekasi Timur', '2001/10/28','Male', '082151842449', 'Bidc920'
+exec Input_Customer 'CU012', 'Alustri Oktrum', 'alustri.oktrum@binus.ac.id', 'Tangerang Barat', '1997/02/10','Female', '081711372576', 'jUdi934'
+exec Input_Customer 'CU013', 'Callista Triadmaja', 'callista.triadmaja@binus.ac.id', 'Surabaya', '1997/05/22','Female', '082117874768', 'maIn732'
+exec Input_Customer 'CU014', 'Satria Nometri', 'satria.nometri@binus.ac.id', 'Monas', '1999/09/11','Male', '082142105162', 'maiN009'
+exec Input_Customer 'CU015', 'Indra Gazali', 'indra.gazali@binus.ac.id', 'Ancol', '2003/03/29','Male', '082157470851', 'ball100'
+GO
+
+delete from Customer
